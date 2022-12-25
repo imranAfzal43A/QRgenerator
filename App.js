d126972026f11456233f9d6dd20df7f4441fe21d
imranAfzal43A <muhammadimran43a@gmail.com>
2022-12-25 21:40:43 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import QR from './components/qrCodegen';
 import React from 'react';
-import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
-
-const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
-
 export default function App() {
   return (
     <>
       <QR />
       <StatusBar style="auto" />
-      <BannerAd
-        unitId={adUnitId}
-        size={BannerAdSize.FULL_BANNER}
-        requestOptions={{
-          requestNonPersonalizedAdsOnly: true,
-        }}
-      />
     </>
   );
 }
